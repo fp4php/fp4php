ef8b5b5950506ea6b6bbe1c65a48d13df92014f5
andrew <klimichkartorgnusov@gmail.com>
2023-03-30 18:22:47 +0300

    test: Cover ArrayList module with tests

---
+++ tests/Module/ArrayListTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fp4\PHP\Test\Module;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Fp4\PHP\Module\ArrayList as L;
+use function Fp4\PHP\Module\Functions\pipe;
+use function PHPUnit\Framework\assertEquals;
+
+final class ArrayListTest extends TestCase
+{
+    #[Test]
+    public static function map(): void
+    {
+        $addOne = fn(int $i): int => $i + 1;
+
+        assertEquals([], pipe(
+            [],
+            L\map($addOne),
+        ));
+        assertEquals([2, 3, 4], pipe(
+            [1, 2, 3],
+            L\map($addOne),
+        ));
+    }
+
+    #[Test]
+    public static function mapKV(): void
+    {
+        $joinWithIndex = fn(int $index, string $offset): string => "{$index}-{$offset}";
+
+        assertEquals([], pipe(
+            [],
+            L\mapKV($joinWithIndex),
+        ));
+        assertEquals(['0-fst', '1-snd', '2-thr'], pipe(
+            ['fst', 'snd', 'thr'],
+            L\mapKV($joinWithIndex),
+        ));
+    }
+
+    #[Test]
+    public static function flatMap(): void
+    {
+        $getSiblings = fn(int $i): array => [$i - 1, $i, $i + 1];
+
+        assertEquals([], pipe(
+            [],
+            L\flatMap($getSiblings),
+        ));
+        assertEquals([0, 1, 2, 1, 2, 3, 2, 3, 4], pipe(
+            [1, 2, 3],
+            L\flatMap($getSiblings),
+        ));
+    }
+
+    #[Test]
+    public static function flatMapKV(): void
+    {
+        $getSiblingsAtPosition = fn(int $position, int $i): array => [
+            sprintf('%s:[%s]', $position, $i - 1),
+            sprintf('%s:[%s]', $position, $i),
+            sprintf('%s:[%s]', $position, $i + 1),
+        ];
+
+        assertEquals([], pipe(
+            [],
+            L\flatMapKV($getSiblingsAtPosition),
+        ));
+        assertEquals(['0:[0]', '0:[1]', '0:[2]', '1:[1]', '1:[2]', '1:[3]', '2:[2]', '2:[3]', '2:[4]'], pipe(
+            [1, 2, 3],
+            L\flatMapKV($getSiblingsAtPosition),
+        ));
+    }
+
+    #[Test]
+    public static function prepend(): void
+    {
+        assertEquals([42], pipe(
+            [],
+            L\prepend(42),
+        ));
+        assertEquals([42, 43, 44], pipe(
+            [43, 44],
+            L\prepend(42),
+        ));
+    }
+
+    #[Test]
+    public static function append(): void
+    {
+        assertEquals([42], pipe(
+            [],
+            L\append(42),
+        ));
+        assertEquals([40, 41, 42], pipe(
+            [40, 41],
+            L\append(42),
+        ));
+    }
+}
