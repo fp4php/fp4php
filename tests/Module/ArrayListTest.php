03e79cbce60d1d2bd570bd8e5644541bf74506d0
adrew <klimichkartorgnusov@gmail.com>
2023-04-11 09:51:14 +0300

    chore: Change L\fromIterable to L\from in tests

--- tests/Module/ArrayListTest.php
+++ tests/Module/ArrayListTest.php
@@ -40,12 +40,12 @@ final class ArrayListTest extends TestCase
         $addOne = fn (int $i): int => $i + 1;
 
         assertEquals([], pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\map($addOne),
         ));
 
         assertEquals([2, 3, 4], pipe(
-            L\fromIterable([1, 2, 3]),
+            L\from([1, 2, 3]),
             L\map($addOne),
         ));
     }
@@ -56,12 +56,12 @@ final class ArrayListTest extends TestCase
         $joinWithIndex = fn (int $index, string $offset): string => "{$index}-{$offset}";
 
         assertEquals([], pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\mapKV($joinWithIndex),
         ));
 
         assertEquals(['0-fst', '1-snd', '2-thr'], pipe(
-            L\fromIterable(['fst', 'snd', 'thr']),
+            L\from(['fst', 'snd', 'thr']),
             L\mapKV($joinWithIndex),
         ));
     }
@@ -72,12 +72,12 @@ final class ArrayListTest extends TestCase
         $getSiblings = fn (int $i): array => [$i - 1, $i, $i + 1];
 
         assertEquals([], pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\flatMap($getSiblings),
         ));
 
         assertEquals([0, 1, 2, 1, 2, 3, 2, 3, 4], pipe(
-            L\fromIterable([1, 2, 3]),
+            L\from([1, 2, 3]),
             L\flatMap($getSiblings),
         ));
     }
@@ -92,12 +92,12 @@ final class ArrayListTest extends TestCase
         ];
 
         assertEquals([], pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\flatMapKV($getSiblingsAtPosition),
         ));
 
         assertEquals(['0:[0]', '0:[1]', '0:[2]', '1:[1]', '1:[2]', '1:[3]', '2:[2]', '2:[3]', '2:[4]'], pipe(
-            L\fromIterable([1, 2, 3]),
+            L\from([1, 2, 3]),
             L\flatMapKV($getSiblingsAtPosition),
         ));
     }
@@ -106,12 +106,12 @@ final class ArrayListTest extends TestCase
     public static function prepend(): void
     {
         assertEquals([42], pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\prepend(42),
         ));
 
         assertEquals([42, 43, 44], pipe(
-            L\fromIterable([43, 44]),
+            L\from([43, 44]),
             L\prepend(42),
         ));
     }
@@ -120,12 +120,12 @@ final class ArrayListTest extends TestCase
     public static function append(): void
     {
         assertEquals([42], pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\append(42),
         ));
 
         assertEquals([40, 41, 42], pipe(
-            L\fromIterable([40, 41]),
+            L\from([40, 41]),
             L\append(42),
         ));
     }
@@ -134,17 +134,17 @@ final class ArrayListTest extends TestCase
     public static function last(): void
     {
         assertEquals(O\none, pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\last(...),
         ));
 
         assertEquals(O\some(1), pipe(
-            L\fromIterable([1]),
+            L\from([1]),
             L\last(...),
         ));
 
         assertEquals(O\some(3), pipe(
-            L\fromIterable([1, 2, 3]),
+            L\from([1, 2, 3]),
             L\last(...),
         ));
     }
@@ -153,17 +153,17 @@ final class ArrayListTest extends TestCase
     public static function first(): void
     {
         assertEquals(O\none, pipe(
-            L\fromIterable([]),
+            L\from([]),
             L\first(...),
         ));
 
         assertEquals(O\some(1), pipe(
-            L\fromIterable([1]),
+            L\from([1]),
             L\first(...),
         ));
 
         assertEquals(O\some(1), pipe(
-            L\fromIterable([1, 2, 3]),
+            L\from([1, 2, 3]),
             L\first(...),
         ));
     }
@@ -176,12 +176,12 @@ final class ArrayListTest extends TestCase
             : O\none;
 
         assertEquals(O\none, pipe(
-            L\fromIterable([1, 2, 3]),
+            L\from([1, 2, 3]),
             L\traverseOption($proveEven),
         ));
 
         assertEquals(O\some([2, 4, 6]), pipe(
-            L\fromIterable([2, 4, 6]),
+            L\from([2, 4, 6]),
             L\traverseOption($proveEven),
         ));
     }
@@ -190,12 +190,12 @@ final class ArrayListTest extends TestCase
     public static function sequenceOption(): void
     {
         assertEquals(O\none, pipe(
-            L\fromIterable([O\some(1), O\some(2), O\none]),
+            L\from([O\some(1), O\some(2), O\none]),
             L\sequenceOption(...),
         ));
 
         assertEquals(O\some([1, 2, 3]), pipe(
-            L\fromIterable([O\some(1), O\some(2), O\some(3)]),
+            L\from([O\some(1), O\some(2), O\some(3)]),
             L\sequenceOption(...),
         ));
     }
