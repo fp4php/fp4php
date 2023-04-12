e2aa0369ff48c0689555a954362aa337fbd7217e
adrew <klimichkartorgnusov@gmail.com>
2023-04-12 22:51:42 +0300

    refactor: BindablePropertiesResolver

--- psalm/Bindable/BindablePropertiesResolver.php
+++ psalm/Bindable/BindablePropertiesResolver.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Fp4\PHP\PsalmIntegration\Bindable;
 
+use Fp4\PHP\Module\ArrayDictionary as D;
+use Fp4\PHP\Module\Evidence as Ev;
 use Fp4\PHP\Module\Option as O;
 use Fp4\PHP\PsalmIntegration\PsalmUtils\PsalmApi;
 use Fp4\PHP\Type\Bindable;
@@ -18,7 +20,6 @@ use Psalm\Type\Atomic\TGenericObject;
 use Psalm\Type\Atomic\TObjectWithProperties;
 use Psalm\Type\Union;
 
-use function Fp4\PHP\Module\Evidence\proveOf;
 use function Fp4\PHP\Module\Functions\constNull;
 use function Fp4\PHP\Module\Functions\pipe;
 
@@ -26,40 +27,33 @@ final class BindablePropertiesResolver implements AfterExpressionAnalysisInterfa
 {
     public static function afterExpressionAnalysis(AfterExpressionAnalysisEvent $event): ?bool
     {
-        $expr = $event->getExpr();
         $source = $event->getStatementsSource();
         $context = $event->getContext();
 
-        if (Bindable::class === $context->self) {
-            return null;
-        }
-
-        if (!$expr instanceof PropertyFetch) {
-            return null;
-        }
-
         return pipe(
-            $expr->var,
-            PsalmApi::$types->getExprType($event),
-            O\flatMap(self::foldBindableContext(...)),
-            O\flatMap(fn (array $ctx) => pipe(
+            O\some($event->getExpr()),
+            O\filter(fn () => Bindable::class === $context->self),
+            Ev\proveOf(PropertyFetch::class),
+            O\flatMap(fn (PropertyFetch $expr) => pipe(
                 $expr->name,
-                proveOf(Identifier::class),
+                Ev\proveOf(Identifier::class),
                 O\map(fn (Identifier $i) => $i->toString()),
-                O\flatMap(function (string $property) use ($ctx, $expr, $source) {
-                    if (!isset($ctx[$property])) {
+                O\flatMap(fn (string $property) => pipe(
+                    $expr->var,
+                    PsalmApi::$types->getExprType($event),
+                    O\flatMap(self::foldBindableContext(...)),
+                    O\flatMap(D\get($property)),
+                    O\orElse(function () use ($expr, $source) {
                         IssueBuffer::maybeAdd(
-                            PropertyIsNotDefinedInScope::create($expr, $source),
-                            $source->getSuppressedIssues(),
+                            e: PropertyIsNotDefinedInScope::create($expr, $source),
+                            suppressed_issues: $source->getSuppressedIssues(),
                         );
 
                         return O\none;
-                    }
-
-                    return O\some($ctx[$property]);
-                }),
+                    }),
+                )),
             )),
-            O\tap(PsalmApi::$types->setType($expr, $event)),
+            O\tap(PsalmApi::$types->setType($event->getExpr(), $event)),
             constNull(...),
         );
     }
@@ -72,7 +66,7 @@ final class BindablePropertiesResolver implements AfterExpressionAnalysisInterfa
         return pipe(
             $type,
             PsalmApi::$types->asSingleAtomic(...),
-            O\flatMap(proveOf(TGenericObject::class)),
+            O\flatMap(Ev\proveOf(TGenericObject::class)),
             O\filter(fn (TGenericObject $bindable) => Bindable::class === $bindable->value),
             O\map(fn (TGenericObject $bindable) => pipe(
                 O\fromNullable($bindable->extra_types['object'] ?? null),
