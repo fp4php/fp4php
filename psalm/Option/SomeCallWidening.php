68fb9ac6bbe6ee7ece46dca4e186866c24e0a0c5
adrew <klimichkartorgnusov@gmail.com>
2023-04-15 15:13:30 +0300

    feat: Widening type for O\fromNullable

--- psalm/Option/SomeCallWidening.php
+++ psalm/Option/SomeCallWidening.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Fp4\PHP\PsalmIntegration\Option;
 
 use Fp4\PHP\Module\Option as O;
+use Fp4\PHP\Module\ArrayList as L;
 use Fp4\PHP\PsalmIntegration\PsalmUtils\PsalmApi;
 use PhpParser\Node\Expr\FuncCall;
 use Psalm\Plugin\EventHandler\AfterExpressionAnalysisInterface;
@@ -17,13 +18,17 @@ use function Fp4\PHP\Module\Functions\pipe;
 final class SomeCallWidening implements AfterExpressionAnalysisInterface
 {
     private const SOME = 'Fp4\PHP\Module\Option\some';
+    private const FROM_NULLABLE = 'Fp4\PHP\Module\Option\fromNullable';
 
     public static function afterExpressionAnalysis(AfterExpressionAnalysisEvent $event): ?bool
     {
         return pipe(
             $event->getExpr(),
             proveOf(FuncCall::class),
-            O\filter(fn (FuncCall $c) => self::SOME === $c->name->getAttribute('resolvedName')),
+            O\filter(fn (FuncCall $c) => pipe(
+                L\fromIterable([self::SOME, self::FROM_NULLABLE]),
+                L\contains($c->name->getAttribute('resolvedName')),
+            )),
             O\filter(fn (FuncCall $c) => !$c->isFirstClassCallable()),
             O\flatMap(PsalmApi::$types->getExprType($event)),
             O\map(PsalmApi::$types->asNonLiteralType(...)),
