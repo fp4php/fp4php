8345649ca5237bfeb964ed30152750ec457241fd
andrew <klimichkartorgnusov@gmail.com>
2023-07-01 17:10:09 +0300

    psalm(Option): Widen O\when and O\tryCatch calls

--- psalm/Module/Option/SomeCallInference.php
+++ psalm/Module/Option/SomeCallInference.php
@@ -16,6 +16,8 @@ use function Fp4\PHP\Module\Functions\pipe;
 final class SomeCallInference implements AfterExpressionAnalysisInterface
 {
     private const SOME = 'Fp4\PHP\Module\Option\some';
+    private const WHEN = 'Fp4\PHP\Module\Option\when';
+    private const TRY_CATCH = 'Fp4\PHP\Module\Option\tryCatch';
     private const FROM_NULLABLE = 'Fp4\PHP\Module\Option\fromNullable';
     private const FROM_LITERAL = 'Fp4\PHP\Module\Option\fromLiteral';
     private const FROM_NULLABLE_LITERAL = 'Fp4\PHP\Module\Option\fromNullableLiteral';
@@ -24,7 +26,7 @@ final class SomeCallInference implements AfterExpressionAnalysisInterface
     {
         return pipe(
             O\first(
-                fn() => Widening::widen($event, [self::SOME, self::FROM_NULLABLE]),
+                fn() => Widening::widen($event, [self::SOME, self::FROM_NULLABLE, self::WHEN, self::TRY_CATCH]),
                 fn() => Narrowing::assertNarrowed($event, [self::FROM_LITERAL, self::FROM_NULLABLE_LITERAL]),
             ),
             constNull(...),
