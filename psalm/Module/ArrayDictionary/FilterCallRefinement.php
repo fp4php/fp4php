5b7540851f22d7315dbad158ff77dc3b348dd71c
andrew <klimichkartorgnusov@gmail.com>
2023-07-02 23:58:09 +0300

    chore: cs-fix

--- psalm/Module/ArrayDictionary/FilterCallRefinement.php
+++ psalm/Module/ArrayDictionary/FilterCallRefinement.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Fp4\PHP\PsalmIntegration\Module\ArrayDictionary;
 
 use Closure;
-use Fp4\PHP\Module\Option as O;
 use Fp4\PHP\Module\ArrayList as L;
+use Fp4\PHP\Module\Option as O;
 use Fp4\PHP\PsalmIntegration\PsalmUtils\PsalmApi;
 use Fp4\PHP\PsalmIntegration\PsalmUtils\Refinement\FilterRefinement;
 use Fp4\PHP\PsalmIntegration\PsalmUtils\Refinement\RefinementType;
