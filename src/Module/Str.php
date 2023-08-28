6ad11899193aeef17cff80cd33ffdad6f2729154
andrew <klimichkartorgnusov@gmail.com>
2023-08-28 14:54:41 +0300

    feat(Str): New functions

--- src/Module/Str.php
+++ src/Module/Str.php
@@ -6,6 +6,38 @@ namespace Fp4\PHP\Module\Str;
 
 use Closure;
 
+use function trim as nativeTrim;
+
+function from(string $string): string
+{
+    return $string;
+}
+
+/**
+ * @param non-empty-string $string
+ * @return non-empty-string
+ */
+function fromNonEmpty(string $string): string
+{
+    return $string;
+}
+
+/**
+ * @template TIn of string
+ * @template TSuffix of string
+ *
+ * @param TSuffix $suffix
+ * @psalm-return (Closure(TIn): (
+ *     TIn is non-empty-string ? non-empty-string :
+ *     TSuffix is non-empty-string ? non-empty-string :
+ *     string
+ * ))
+ */
+function append(string $suffix): Closure
+{
+    return fn(string $value) => "{$value}{$suffix}";
+}
+
 /**
  * @template TIn of string
  * @template TPrefix of string
@@ -22,16 +54,86 @@ function prepend(string $prefix): Closure
     return fn(string $value) => "{$prefix}{$value}";
 }
 
-function from(string $string): string
+/**
+ * @return Closure(string): bool
+ */
+function startsWith(string $needle): Closure
 {
-    return $string;
+    return fn(string $haystack) => str_starts_with($haystack, $needle);
 }
 
 /**
- * @param non-empty-string $string
- * @return non-empty-string
+ * @return Closure(string): bool
  */
-function fromNonEmpty(string $string): string
+function endsWith(string $needle): Closure
 {
-    return $string;
+    return fn(string $haystack) => str_ends_with($haystack, $needle);
+}
+
+/**
+ * @return Closure(string): bool
+ */
+function contains(string $needle): Closure
+{
+    return fn(string $haystack) => str_contains($haystack, $needle);
+}
+
+function isEmpty(string $value): bool
+{
+    return '' === $value;
+}
+
+function length(string $value): int
+{
+    return mb_strlen($value);
+}
+
+/**
+ * @return Closure(string): string
+ */
+function replace(string $search, string $replace): Closure
+{
+    return fn(string $subject) => str_replace($search, $replace, $subject);
+}
+
+/**
+ * @return Closure(string): string
+ */
+function substring(int $start, int $length): Closure
+{
+    return fn(string $subject) => mb_substr($subject, $start, $length);
+}
+
+/**
+ * @param non-empty-string $separator
+ * @return Closure(string): non-empty-list<string>
+ */
+function split(string $separator): Closure
+{
+    return fn(string $subject) => explode($separator, $subject);
+}
+
+function toLowerCase(string $subject): string
+{
+    return strtolower($subject);
+}
+
+function toUpperCase(string $subject): string
+{
+    return strtoupper($subject);
+}
+
+function trim(string $subject): string
+{
+    return nativeTrim($subject);
+}
+
+function trimLeft(string $subject): string
+{
+    return ltrim($subject);
+}
+
+function trimRight(string $subject): string
+{
+    return rtrim($subject);
 }
--- tests/Module/StrTest.php
+++ tests/Module/StrTest.php
@@ -35,6 +35,17 @@ final class StrTest extends TestCase
     }
 
     #[Test]
+    public static function append(): void
+    {
+        pipe(
+            Str\from('val'),
+            Str\append('-suffix'),
+            Type\isSameAs('non-empty-string'),
+            Assert\same('val-suffix'),
+        );
+    }
+
+    #[Test]
     public static function prepend(): void
     {
         pipe(
@@ -42,6 +53,268 @@ final class StrTest extends TestCase
             Str\prepend('pref-'),
             Type\isSameAs('non-empty-string'),
             Assert\same('pref-val'),
+        );
+    }
+
+    #[Test]
+    public static function startsWith(): void
+    {
+        pipe(
+            Str\from('val'),
+            Str\startsWith('v'),
+            Type\isSameAs('bool'),
+            Assert\same(true),
+        );
+
+        pipe(
+            Str\from('val'),
+            Str\startsWith('a'),
+            Type\isSameAs('bool'),
+            Assert\same(false),
+        );
+    }
+
+    #[Test]
+    public static function endsWith(): void
+    {
+        pipe(
+            Str\from('val'),
+            Str\endsWith('l'),
+            Type\isSameAs('bool'),
+            Assert\same(true),
+        );
+
+        pipe(
+            Str\from('val'),
+            Str\startsWith('a'),
+            Type\isSameAs('bool'),
+            Assert\same(false),
+        );
+    }
+
+    #[Test]
+    public static function contains(): void
+    {
+        pipe(
+            Str\from('value'),
+            Str\contains('val'),
+            Type\isSameAs('bool'),
+            Assert\same(true),
+        );
+
+        pipe(
+            Str\from('value'),
+            Str\contains('other'),
+            Type\isSameAs('bool'),
+            Assert\same(false),
+        );
+    }
+
+    #[Test]
+    public static function isEmpty_(): void
+    {
+        pipe(
+            Str\from('value'),
+            Str\isEmpty(...),
+            Type\isSameAs('bool'),
+            Assert\same(false),
+        );
+
+        pipe(
+            Str\from(''),
+            Str\isEmpty(...),
+            Type\isSameAs('bool'),
+            Assert\same(true),
+        );
+    }
+
+    #[Test]
+    public static function length(): void
+    {
+        pipe(
+            Str\from(''),
+            Str\length(...),
+            Type\isSameAs('int'),
+            Assert\same(0),
+        );
+
+        pipe(
+            Str\from('abc'),
+            Str\length(...),
+            Type\isSameAs('int'),
+            Assert\same(3),
+        );
+    }
+
+    #[Test]
+    public static function replace(): void
+    {
+        pipe(
+            Str\from('abc'),
+            Str\replace('ab', 'bc'),
+            Type\isSameAs('string'),
+            Assert\same('bcc'),
+        );
+
+        pipe(
+            Str\from('a b c'),
+            Str\replace(' ', ''),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+    }
+
+    #[Test]
+    public static function substring(): void
+    {
+        pipe(
+            Str\from('abc'),
+            Str\substring(0, 2),
+            Type\isSameAs('string'),
+            Assert\same('ab'),
+        );
+    }
+
+    #[Test]
+    public static function split(): void
+    {
+        pipe(
+            Str\from('a, b, c'),
+            Str\split(', '),
+            Type\isSameAs('non-empty-list<string>'),
+            Assert\same(['a', 'b', 'c']),
+        );
+
+        pipe(
+            Str\from(''),
+            Str\split(', '),
+            Type\isSameAs('non-empty-list<string>'),
+            Assert\same(['']),
+        );
+    }
+
+    #[Test]
+    public static function toUpperCase(): void
+    {
+        pipe(
+            Str\from('abc'),
+            Str\toUpperCase(...),
+            Type\isSameAs('string'),
+            Assert\same('ABC'),
+        );
+
+        pipe(
+            Str\from('Abc'),
+            Str\toUpperCase(...),
+            Type\isSameAs('string'),
+            Assert\same('ABC'),
+        );
+
+        pipe(
+            Str\from('ABC'),
+            Str\toUpperCase(...),
+            Type\isSameAs('string'),
+            Assert\same('ABC'),
+        );
+    }
+
+    #[Test]
+    public static function toLowerCase(): void
+    {
+        pipe(
+            Str\from('ABC'),
+            Str\toLowerCase(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+
+        pipe(
+            Str\from('aBC'),
+            Str\toLowerCase(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+
+        pipe(
+            Str\from('abc'),
+            Str\toLowerCase(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+    }
+
+    #[Test]
+    public static function trim(): void
+    {
+        pipe(
+            Str\from(' abc'),
+            Str\trim(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+
+        pipe(
+            Str\from(' abc '),
+            Str\trim(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+
+        pipe(
+            Str\from('  abc  '),
+            Str\trim(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+    }
+
+    #[Test]
+    public static function trimLeft(): void
+    {
+        pipe(
+            Str\from(' abc'),
+            Str\trimLeft(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+
+        pipe(
+            Str\from(' abc '),
+            Str\trimLeft(...),
+            Type\isSameAs('string'),
+            Assert\same('abc '),
+        );
+
+        pipe(
+            Str\from('  abc  '),
+            Str\trimLeft(...),
+            Type\isSameAs('string'),
+            Assert\same('abc  '),
+        );
+    }
+
+    #[Test]
+    public static function trimRight(): void
+    {
+        pipe(
+            Str\from('abc '),
+            Str\trimRight(...),
+            Type\isSameAs('string'),
+            Assert\same('abc'),
+        );
+
+        pipe(
+            Str\from(' abc '),
+            Str\trimRight(...),
+            Type\isSameAs('string'),
+            Assert\same(' abc'),
+        );
+
+        pipe(
+            Str\from('  abc  '),
+            Str\trimRight(...),
+            Type\isSameAs('string'),
+            Assert\same('  abc'),
         );
     }
 }
